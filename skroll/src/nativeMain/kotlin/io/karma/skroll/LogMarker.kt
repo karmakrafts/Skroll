052d9066dd674ff060765dec2c4219cc29ab7158
KitsuneAlex <alexh@karmakrafts.dev>
2025-01-26 17:59:50 +0100

    Add documentation to LogMarker

--- skroll/src/nativeMain/kotlin/io/karma/skroll/LogMarker.kt
+++ skroll/src/nativeMain/kotlin/io/karma/skroll/LogMarker.kt
@@ -18,17 +18,45 @@
 
 package io.karma.skroll
 
+import co.touchlab.stately.collections.SharedHashMap
+
+/**
+ * Represents a log marker to tag messages with to allow
+ * operations like filtering of the messages.
+ */
 interface LogMarker {
     companion object {
-        inline fun create( // @formatter:off
-            key: String,
-            name: String = key,
-            isEnabled: Boolean = true
-        ): LogMarker = SimpleLogMarker(key, name, isEnabled) // @formatter:on
+        @PublishedApi
+        internal val markers: SharedHashMap<String, LogMarker> = SharedHashMap()
+
+        /**
+         * Create a default log marker which is enabled by default.
+         *
+         * @param key The key the marker is identified by.
+         * @param name The name of the marker that is actually printed when {{marker}} is used.
+         * @param isEnabled When true means that all messages with this marker will be logged.
+         * @return A new log marker instance with the given key, name and state.
+         */
+        inline fun create(
+            key: String, name: String = key, isEnabled: Boolean = true
+        ): LogMarker = markers.getOrPut(key) {
+            SimpleLogMarker(key, name, isEnabled)
+        }
     }
 
+    /**
+     * The internal key the marker is referenced by in cache.
+     */
     val key: String
+
+    /**
+     * The name of the marker that is printed when {{marker}} is used.
+     */
     val name: String
+
+    /**
+     * When true means that all messages with this marker will be logged.
+     */
     var isEnabled: Boolean
 }
 
